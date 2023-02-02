916fb3b6adc2b7327ac5fb20d6855aa3927a5da4
RaymondLoranger <Raymond.Loranger@bell.net>
2023-02-02 18:56:30 -0500

    0.1.27

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Grid.MixProject do
   def project do
     [
       app: :islands_grid,
-      version: "0.1.26",
+      version: "0.1.27",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Grid",
